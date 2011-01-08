e2037950ee9f675d3847bf5952ae6b8a92423be5
Bob Somers <bob@bobsomers.com>
2011-01-08 03:27:15 -0800

    Much happier with where this is going now.

--- main.lua
+++ main.lua
@@ -1,116 +1,148 @@
 require "hump/vector"
 
--- ball information
+-- info about the arena walls
+arena = {
+	left = {},
+	right = {},
+	top = {},
+	bottom = {},
+}
+
+-- data about the ball
 ball = {
-	pos = vector(500, 500),
-	size = vector(32, 32),
-	speed = vector(1, -1):normalize_inplace() * 150,
-	img = {}
+	status = "waiting"
 }
 
--- block data
-blocks = {
-	size = vector(80, 20),
-	img = {}
-}
+-- data about our foes
+foes = {}
 
--- game load
+-- data about the goal
+goal = {}
+
+-- focus of the launch vector
+launch_focus = vector(-1, -1)
+
 function love.load()
-	-- load graphics resources
-	ball.img.cry = love.graphics.newImage("ball/cry.png")
-	blocks.img.red = love.graphics.newImage("blocks/red.png")
-	blocks.img.green = love.graphics.newImage("blocks/green.png")
-	blocks.img.blue = love.graphics.newImage("blocks/blue.png")
-	
-	-- layout the blocks
-	layout_blocks()
+	-- randomize the madness
+	math.randomseed(love.timer.getMicroTime())
 	
 	-- set background color
 	love.graphics.setBackgroundColor(255, 255, 255)
+	
+	-- create a new physics world
+	world = love.physics.newWorld(0, 0, 800, 600)
+	world:setGravity(0, 0)
+	world:setMeter(48)
+	
+	-- define extents of the arena	
+	arena.left.body = love.physics.newBody(world, 0, 300, 0, 0)
+	arena.left.shape = love.physics.newRectangleShape(arena.left.body, 0, 0, 5, 600, 0)
+	arena.right.body = love.physics.newBody(world, 800, 300, 0, 0)
+	arena.right.shape = love.physics.newRectangleShape(arena.right.body, 0, 0, 5, 600, 0)
+	arena.top.body = love.physics.newBody(world, 400, 0, 0, 0)
+	arena.top.shape = love.physics.newRectangleShape(arena.top.body, 0, 0, 800, 5, 0)
+	arena.bottom.body = love.physics.newBody(world, 400, 600, 0, 0)
+	arena.bottom.shape = love.physics.newRectangleShape(arena.bottom.body, 0, 0, 800, 5, 0)
+	
+	-- ball settings
+	ball.body = love.physics.newBody(world, 400, 300, 15, 10)
+	ball.body:setLinearDamping(0.3)
+	ball.body:setAngularDamping(0.3)
+	ball.shape = love.physics.newCircleShape(ball.body, 0, 0, 24)
+	ball.shape:setRestitution(0.5)
+	ball.img = love.graphics.newImage("ball/cry.png")
+	
+	-- pick a random sector for the goal
+	local randX = math.random(4)
+	local randY = math.random(3)
+	goal.sector = randX * randY
+	goal.body = love.physics.newBody(world, 100 + ((randX - 1) * 200), 100 + ((randY - 1) * 200), 0, 0)
+	goal.shape = love.physics.newRectangleShape(goal.body, 0, 0, 150, 150, 0)
+	goal.shape:setSensor(true)
 end
 
--- update
 function love.update(dt)
-	-- move the ball
-	ball.pos = ball.pos + (ball.speed * dt)
+	-- update the physics world
+	world:update(dt)
 	
-	-- check for collisions with the walls
-	if ball.pos.x < 0 then
-		ball.pos.x = 0
-		ball.speed.x = ball.speed.x * -1
-	elseif ball.pos.x + ball.size.x > 800 then
-		ball.pos.x = 800 - ball.size.x
-		ball.speed.x = ball.speed.x * -1
-	end
-	if ball.pos.y < 0 then
-		ball.pos.y = 0
-		ball.speed.y = ball.speed.y * -1
-	elseif ball.pos.y + ball.size.y > 600 then
-		ball.pos.y = 600 - ball.size.y
-		ball.speed.y = ball.speed.y * -1
-	end
-	
-	-- check for collisions with the blocks
-	for i = 0, 9 do
-		for j = 0, 4 do
-			-- calculate which block, as well as its x and y position
-			local index = i + (10 * j)
-			local x = i * blocks.size.x
-			local y = j * blocks.size.y
-			
-			-- only check collisions if block exists
-			if blocks[index] ~= 0 then
-				-- bottom, top, left, right
-				if ball.pos.y < y + blocks.size.y and (ball.pos.x < x + blocks.size.x or ball.pos.x + ball.size.x > x) then
-					ball.pos.y = y + blocks.size.y
-					ball.speed.y = ball.speed.y * -1
-				elseif ball.pos.y + ball.size.y > y and (ball.pos.x < x + blocks.size.x or ball.pos.x + ball.size.x > x) then
-					ball.pos.y = y - ball.size.y
-					ball.speed.y = ball.speed.y * -1
-				elseif ball.pos.x < x + blocks.size.x and (ball.pos.y < y + blocks.size.y or ball.pos.y + ball.size.y > y) then
-					ball.pos.x = x + blocks.size.x
-					ball.speed.x = ball.speed.x * -1
-				elseif ball.pos.x + ball.size.x > x and (ball.pos.y < y + blocks.size.y or ball.pos.y + ball.size.y > y) then
-					ball.pos.x = x - ball.size.x
-					ball.speed.x = ball.speed.x * -1]]--
-				end
-			end
-		end
+	-- has the ball come to rest?
+	local x, y = ball.body:getLinearVelocity()
+	local speed = vector(x, y):len()
+	if speed > 0 and speed < 2 then
+		ball.status = "waiting"
+		ball.body:setLinearVelocity(0, 0)
+		ball.shape:setSensor(true)
 	end
 end
 
--- draw
 function love.draw()
-	-- draw the blocks
-	for i = 0, 9 do
-		for j = 0, 4 do
-			-- calculate which block, as well as its x and y position
-			local index = i + (10 * j)
-			local x = i * blocks.size.x
-			local y = j * blocks.size.y
-			
-			if blocks[index] == 1 then -- red
-				love.graphics.draw(blocks.img.red, x, y)
-			elseif blocks[index] == 2 then -- green
-				love.graphics.draw(blocks.img.green, x, y)
-			elseif blocks[index] == 3 then -- blue
-				love.graphics.draw(blocks.img.blue, x, y)
-			end
-		end
+	-- draw goal
+	local x1, y1, x2, y2, x3, y3, x4, y4 = goal.shape:getBoundingBox()
+	local boxW = x3 - x2
+	local boxH = y2 - y1
+	love.graphics.setColor(200, 255, 200)
+	love.graphics.rectangle("fill", goal.body:getX() - (boxW / 2), goal.body:getY() - (boxH / 2), boxW, boxH)
+	love.graphics.setColor(0, 255, 0)
+	love.graphics.setLine(2, "smooth")
+	love.graphics.rectangle("line", goal.body:getX() - (boxW / 2), goal.body:getY() - (boxH / 2), boxW, boxH)
+
+	-- draw launch vector
+	if launch_focus.x > -1 then
+		love.graphics.setColor(255, 0, 0)
+		love.graphics.setLine(3, "smooth")
+		love.graphics.line(launch_focus.x, launch_focus.y, love.mouse.getX(), love.mouse.getY())
 	end
 
-	-- draw the ball
-	love.graphics.draw(ball.img.cry, ball.pos.x, ball.pos.y)
+	-- draw ball
+	if ball.status ~= "waiting" then
+		local x = 0
+		local y = 0
+		
+		if ball.status == "prepping" then
+			x = love.mouse.getX()
+			y = love.mouse.getY()
+			love.graphics.setColor(255, 255, 255, 127)
+		else
+			x = ball.body:getX()
+			y = ball.body:getY()
+			love.graphics.setColor(255, 255, 255, 255)
+		end
+		
+		love.graphics.draw(ball.img, x, y, ball.body:getAngle(), 1, 1, 24, 24)
+	end
 end
 
--- layout blocks
-function layout_blocks()
-	for i = 0, 9 do
-		for j = 0, 4 do
-			local index = i + (10 * j)
-			local clr = (index % 3) + 1
-			
-			blocks[index] = clr
-		end
+function love.mousepressed(x, y, button)
+	if button == "l" then
+		-- prep ball for launch
+		launch_focus.x = x
+		launch_focus.y = y
+		ball.status = "prepping"
+		ball.shape:setSensor(true)
+		ball.body:setLinearVelocity(0, 0)
+		ball.body:setAngularVelocity(0)
+		ball.body:setAngle(0)
+	elseif button == "r" and launch_focus.x > -1 then
+		-- cancel launch
+		launch_focus.x = -1
+		launch_focus.y = -1
+		ball.status = "waiting"
+		ball.shape:setSensor(true)
+	end
+end
+
+function love.mousereleased(x, y, button)
+	if button == "l" and launch_focus.x > -1 then
+		-- successful launch!
+		ball.status = "launched"
+		ball.shape:setSensor(false)
+		ball.body:setX(x)
+		ball.body:setY(y)
+		
+		local impulse = vector(launch_focus.x - x, launch_focus.y - y) / 2
+		ball.body:applyImpulse(impulse.x, impulse.y)
+		
+		launch_focus.x = -1
+		launch_focus.y = -1
 	end
 end

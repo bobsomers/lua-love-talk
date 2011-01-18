13660179ef17eafcf8bb4c38c6cc9b2b2cefd705
Bob Somers <bob@bobsomers.com>
2011-01-17 22:36:15 -0800

    Revamping things... again. Rage curling is it this time, I swear. Got a curling field (court? arena?) up and rolling, with camera controls (right click translation and mouse wheel scaling).

--- main.lua
+++ main.lua
@@ -1,71 +1,106 @@
 require "hump/vector"
+require "hump/camera"
 
--- info about the arena walls
-arena = {
+-- "constants"
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+ARENA_WIDTH = 2400
+ARENA_HEIGHT = 200
+
+-- data about the walls
+walls = {
 	left = {},
 	right = {},
 	top = {},
 	bottom = {},
 }
 
--- data about the ball
-ball = {
-	status = "waiting"
+-- data about the house
+house = {
+	radius = 75
 }
 
--- data about our foes
-foes = {}
-
--- data about the goal
-goal = {}
-
--- focus of the launch vector
-launch_focus = vector(-1, -1)
+-- data about the stones
+stones = {}
 
 function love.load()
 	-- randomize the madness
-	math.randomseed(love.timer.getMicroTime())
+	--math.randomseed(love.timer.getMicroTime())
 	
 	-- set background color
-	love.graphics.setBackgroundColor(255, 255, 255)
+	love.graphics.setBackgroundColor(220, 220, 220)
+	
+	-- camera
+	cam = Camera(vector(SCREEN_WIDTH / 4, ARENA_HEIGHT / 2))
+	cam.moving = false
+	cam.lastCoords = vector(-1, -1)
 	
 	-- create a new physics world
-	world = love.physics.newWorld(0, 0, 800, 600)
+	world = love.physics.newWorld(0, 0, ARENA_WIDTH, ARENA_HEIGHT)
 	world:setGravity(0, 0)
 	world:setMeter(48)
 	
-	-- define extents of the arena	
-	arena.left.body = love.physics.newBody(world, 0, 300, 0, 0)
-	arena.left.shape = love.physics.newRectangleShape(arena.left.body, 0, 0, 5, 600, 0)
-	arena.right.body = love.physics.newBody(world, 800, 300, 0, 0)
-	arena.right.shape = love.physics.newRectangleShape(arena.right.body, 0, 0, 5, 600, 0)
-	arena.top.body = love.physics.newBody(world, 400, 0, 0, 0)
-	arena.top.shape = love.physics.newRectangleShape(arena.top.body, 0, 0, 800, 5, 0)
-	arena.bottom.body = love.physics.newBody(world, 400, 600, 0, 0)
-	arena.bottom.shape = love.physics.newRectangleShape(arena.bottom.body, 0, 0, 800, 5, 0)
+	-- define our walls
+	walls.left.body = love.physics.newBody(world, 2, ARENA_HEIGHT / 2, 0, 0)
+	walls.left.shape = love.physics.newRectangleShape(walls.left.body, 0, 0, 5, ARENA_HEIGHT, 0)
+	walls.right.body = love.physics.newBody(world, ARENA_WIDTH - 2, ARENA_HEIGHT / 2, 0, 0)
+	walls.right.shape = love.physics.newRectangleShape(walls.right.body, 0, 0, 5, ARENA_HEIGHT, 0)
+	walls.top.body = love.physics.newBody(world, ARENA_WIDTH / 2, 2, 0, 0)
+	walls.top.shape = love.physics.newRectangleShape(walls.top.body, 0, 0, ARENA_WIDTH, 5, 0)
+	walls.bottom.body = love.physics.newBody(world, ARENA_WIDTH / 2, ARENA_HEIGHT - 2, 0, 0)
+	walls.bottom.shape = love.physics.newRectangleShape(walls.bottom.body, 0, 0, ARENA_WIDTH, 5, 0)
+	
+	-- define the house
+	house.body = love.physics.newBody(world, ARENA_WIDTH - (3 * house.radius), ARENA_HEIGHT / 2, 0, 0)
+	house.shape = love.physics.newCircleShape(house.body, 0, 0, house.radius)
 	
 	-- ball settings
+	--[[
 	ball.body = love.physics.newBody(world, 400, 300, 15, 10)
 	ball.body:setLinearDamping(0.3)
 	ball.body:setAngularDamping(0.3)
 	ball.shape = love.physics.newCircleShape(ball.body, 0, 0, 24)
 	ball.shape:setRestitution(0.5)
 	ball.img = love.graphics.newImage("ball/cry.png")
+	]]--
 	
 	-- pick a random sector for the goal
+	--[[
 	local randX = math.random(4)
 	local randY = math.random(3)
 	goal.sector = randX * randY
 	goal.body = love.physics.newBody(world, 100 + ((randX - 1) * 200), 100 + ((randY - 1) * 200), 0, 0)
 	goal.shape = love.physics.newRectangleShape(goal.body, 0, 0, 150, 150, 0)
 	goal.shape:setSensor(true)
+	]]--
 end
 
 function love.update(dt)
+	if love.keyboard.isDown("right") then
+		cam:translate(vector(5, 0))
+	end
+	if love.keyboard.isDown("left") then
+		cam:translate(vector(-5, 0))
+	end
+	if love.keyboard.isDown("up") then
+		cam:translate(vector(0, -5))
+	end
+	if love.keyboard.isDown("down") then
+		cam:translate(vector(0, 5))
+	end
+	
+	-- move the camera
+	if cam.moving then
+		local pos = cam:mousepos()
+		local delta = cam.clickPos - pos
+		cam:translate(delta)
+	end
+
 	-- update the physics world
-	world:update(dt)
+	--world:update(dt)
 	
 	-- has the ball come to rest?
+	--[[
 	local x, y = ball.body:getLinearVelocity()
 	local speed = vector(x, y):len()
 	if speed > 0 and speed < 2 then
@@ -73,27 +108,58 @@ function love.update(dt)
 		ball.body:setLinearVelocity(0, 0)
 		ball.shape:setSensor(true)
 	end
+	]]--
+end
+
+function drawPhysicsRectangle(obj)
+	local x1, y1, x2, y2, x3, y3, x4, y4 = obj.shape:getBoundingBox()
+	local w = x3 - x2
+	local h = y2 - y1
+	love.graphics.rectangle("fill", obj.body:getX() - (w / 2), obj.body:getY() - (h / 2), w, h)
 end
 
 function love.draw()
-	-- draw goal
-	local x1, y1, x2, y2, x3, y3, x4, y4 = goal.shape:getBoundingBox()
-	local boxW = x3 - x2
-	local boxH = y2 - y1
-	love.graphics.setColor(200, 255, 200)
-	love.graphics.rectangle("fill", goal.body:getX() - (boxW / 2), goal.body:getY() - (boxH / 2), boxW, boxH)
-	love.graphics.setColor(0, 255, 0)
-	love.graphics.setLine(2, "smooth")
-	love.graphics.rectangle("line", goal.body:getX() - (boxW / 2), goal.body:getY() - (boxH / 2), boxW, boxH)
+	-- convenience
+	local gfx = love.graphics
 
+	-- draw the world
+	cam:predraw()
+
+	-- draw ice
+	gfx.setColor(255, 255, 255)
+	gfx.rectangle("fill", 0, 0, ARENA_WIDTH, ARENA_HEIGHT)
+	
+	-- draw walls
+	gfx.setColor(0, 0, 0)
+	drawPhysicsRectangle(walls.left)
+	drawPhysicsRectangle(walls.right)
+	drawPhysicsRectangle(walls.top)
+	drawPhysicsRectangle(walls.bottom)
+	
+	-- draw house
+	gfx.setColor(0, 0, 255)
+	gfx.circle("fill", house.body:getX(), house.body:getY(), house.radius, 50)
+	gfx.setColor(255, 255, 255)
+	gfx.circle("fill", house.body:getX(), house.body:getY(), house.radius * 2 / 3, 50)
+	gfx.setColor(255, 0, 0)
+	gfx.circle("fill", house.body:getX(), house.body:getY(), house.radius / 3, 25)
+	gfx.setColor(255, 255, 255)
+	gfx.circle("fill", house.body:getX(), house.body:getY(), 5, 10)
+
+	-- done drawing the world
+	cam:postdraw()
+	
 	-- draw launch vector
+	--[[
 	if launch_focus.x > -1 then
 		love.graphics.setColor(255, 0, 0)
 		love.graphics.setLine(3, "smooth")
 		love.graphics.line(launch_focus.x, launch_focus.y, love.mouse.getX(), love.mouse.getY())
 	end
+	]]--
 
 	-- draw ball
+	--[[
 	if ball.status ~= "waiting" then
 		local x = 0
 		local y = 0
@@ -110,9 +176,27 @@ function love.draw()
 		
 		love.graphics.draw(ball.img, x, y, ball.body:getAngle(), 1, 1, 24, 24)
 	end
+	]]--
 end
 
 function love.mousepressed(x, y, button)
+	-- handle zooming
+	if button == "wd" then
+		cam.zoom = cam.zoom - 0.1
+		if cam.zoom < 0.3 then cam.zoom = 0.3 end
+	end
+	if button == "wu" then
+		cam.zoom = cam.zoom + 0.1
+		if cam.zoom > 2.0 then cam.zoom = 2.0 end
+	end
+
+	-- handle right click moving
+	if button == "r" then
+		cam.moving = true
+		cam.clickPos = cam:mousepos()
+	end
+
+	--[[
 	if button == "l" then
 		-- prep ball for launch
 		launch_focus.x = x
@@ -129,9 +213,16 @@ function love.mousepressed(x, y, button)
 		ball.status = "waiting"
 		ball.shape:setSensor(true)
 	end
+	]]--
 end
 
 function love.mousereleased(x, y, button)
+	-- handle right click moving
+	if button == "r" then
+		cam.moving = false
+	end
+
+	--[[
 	if button == "l" and launch_focus.x > -1 then
 		-- successful launch!
 		ball.status = "launched"
@@ -145,4 +236,5 @@ function love.mousereleased(x, y, button)
 		launch_focus.x = -1
 		launch_focus.y = -1
 	end
+	]]--
 end

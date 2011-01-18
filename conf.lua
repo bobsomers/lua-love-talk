50ac51f68bfcfaabebe62d22d96350cab47eac00
Bob Somers <bob@bobsomers.com>
2011-01-17 22:34:59 -0800

    Tweaking the config file a bit.

--- conf.lua
+++ conf.lua
@@ -1,5 +1,13 @@
 function love.conf(t)
+	-- game info
+	t.title = "Rage Curling"
+	t.author = "Bob Somers"
+	
+	-- graphics settings
 	t.screen.width = 800
 	t.screen.height = 600
+	t.screen.fsaa = 3
+	
+	-- debug settings
 	t.console = true
 end

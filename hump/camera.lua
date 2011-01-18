80ec10cdd2422f899da00e6ec42d44b6971f2fc4
Bob Somers <bob@bobsomers.com>
2011-01-17 22:34:32 -0800

    Fixing a file path issue in the camera module now that it's being included in the project directory.

--- hump/camera.lua
+++ hump/camera.lua
@@ -24,7 +24,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 ]]--
 
-require 'vector'
+require 'hump/vector'
 
 local camera = {}
 camera.__index = camera
